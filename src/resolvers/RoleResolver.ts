2d006129cf0e37130672c2d45d0aae3734a7918a
rishabh0504 <rishabh.tiwari@synacor.com>
2020-07-21 04:34:56 +0400

    Input mappers and roles crud written.

--- src/resolvers/RoleResolver.ts
+++ src/resolvers/RoleResolver.ts
@@ -1,13 +1,16 @@
-import { Resolver, Mutation, Arg, Query } from "type-graphql";
+import { Resolver, Mutation, Query, Arg } from "type-graphql";
 import { Role } from "../entity/Role";
+import { RoleInput } from "../mappers/RoleInput";
+
+
 
 
 @Resolver()
 export class RoleResolver {
 
   @Mutation(() => Boolean)     // this argument in function defins th ereturn type of the mutation functions.
-  async createRole(@Arg("roleName") roleName: string) {   // This argument is necessary to tell graphql that argument comming to this resollver is stri8ng type (not mandatory to define)
-    await Role.insert({ roleName })
+  async createRole(@Arg("options", () => RoleInput) options: RoleInput) {   // This argument is necessary to tell graphql that argument comming to this resollver is stri8ng type (not mandatory to define)
+    await Role.insert(options)
     return true;
   }
 
@@ -20,5 +23,17 @@ export class RoleResolver {
       console.error(error);
       return [];
     }
+  }
+
+  @Mutation(() => Boolean)
+  async delete(@Arg("id") id: number) {
+    await Role.delete({ id })
+    return true;
+  }
+
+  @Mutation(() => Boolean)
+  async update(@Arg("id") id: number, @Arg("input", () => RoleInput) input: RoleInput) {
+    await Role.update({ id }, input)
+    return true;
   }
 }
